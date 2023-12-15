62e15377c1fb7efd2f9a4596409cacb987785a82
Vihangi Perera <vihangiperera9@gmail.com>
2023-12-14 21:07:13 -0800

    Basic frame done.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ProjectList from './components/ProjectList';
+import logo from './images/VP Logo.png';
+import bitmoji from './images/VihangiBitmoji.PNG';
+import github from './images/github.png';
+import linkedin from './images/linkedin.png';
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <ProjectList />
-      <Footer />
+      <div className='Header'>
+        <img src={logo} alt="Logo" className="logo"/>
+        
+        <div className='socials'>
+          <a href="https://github.com/Vihangip" target="_blank" rel="noopener noreferrer">
+            <img src={github} alt="Github" className="github"/>
+          </a>
+          <a href="https://www.linkedin.com/in/vihangiperera/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin} alt="Linkedin" className="linkedin"/>
+          </a>
+        </div>
+      </div>
+
+      <div className='Main'>
+        <img src={bitmoji} alt="Bitmoji" className="bitmoji" />
+        <Header />
+      </div>
+  
     </div>
+
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
-      <h1>Welcome to My Personal Website</h1>
-    </header>
+    <div className='header-text'>
+      <p>
+        Hi I'm Vihangi 👋🏼 
+        I am a 4th year student at the University of British Columbia studying Computer Science. <br />
+        I am passionate about many aspects of software development and love creating cool things that help people! 
+      </p>
+    </div>
   );
 }
 
