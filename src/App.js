f00cdd4b93c13ff49f33f7a6ccfb1e3aadfb8ac6
Vihangi Perera <vihangiperera9@gmail.com>
2023-12-14 19:16:49 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProjectList from './components/ProjectList';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ProjectList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
